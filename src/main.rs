53aa418e3e6d19c3bc0205a93a1571c4bc9f05f2
Caio Piccirillo <caiopiccirillo@gmail.com>
2021-08-19 18:04:22 -0300

    First commit

---
+++ src/main.rs
@@ -0,0 +1,53 @@
+use clap::{AppSettings, Clap};
+use std::fs;
+use std::{thread, time};
+
+#[derive(Clap)]
+#[clap(version = env!("CARGO_PKG_VERSION"), author = env!("CARGO_PKG_AUTHORS"))]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Opts {
+    /// Initialize the control
+    #[clap(short, long)]
+    init: bool,
+    /// Level of verbosity
+    #[clap(short, long, default_value = "1")]
+    verbose: i32,
+}
+
+const THERMAL_ZONE: &str = "/sys/devices/virtual/thermal/thermal_zone0/temp";
+const TARGET_PWM: &str = "/sys/devices/pwm-fan/target_pwm";
+const LOW_LIMIT: usize = 25_000;
+const HIGH_LIMIT: usize = 40_000;
+
+fn main() {
+    let opts: Opts = Opts::parse();
+
+    if opts.init {
+        loop {
+            let sleep_duration = time::Duration::from_secs(10);
+            let contents = fs::read_to_string(THERMAL_ZONE)
+                .expect("Something went wrong reading the temperature");
+            let temp = contents.trim().parse::<usize>().unwrap();
+            println!("Temperature: {}", temp);
+            let pwm = map_fan_pwm(temp);
+            println!("PWM: {}", pwm);
+            fs::write(TARGET_PWM, pwm.to_string()).expect("Something went wrong writing to file");
+            thread::sleep(sleep_duration);
+        }
+    }
+
+    // match opts.verbose {
+    //     0 => println!("No verbose info"),
+    //     1 => println!("Some verbose info"),
+    //     2 => println!("Tons of verbose info"),
+    //     _ => println!("Don't be ridiculous"),
+    // }
+}
+
+fn map_fan_pwm(temp: usize) -> usize {
+    match temp {
+        t if t < LOW_LIMIT => 0,
+        t if t > HIGH_LIMIT => 255,
+        _ => (255 * (temp - LOW_LIMIT)) / (HIGH_LIMIT - LOW_LIMIT),
+    }
+}
